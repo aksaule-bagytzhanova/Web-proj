2fcb8e18121fc18acc12886981d79bc6904e3823
Aksaule <aksaule124026@gmail.com>
2021-07-27 19:16:24 +0600

    Create models

---
+++ project/api/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = 
+
--- project/api/models.py
+++ project/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class All(models.Model):
+    user_email = models.EmailField(max_length=254)
+    user_phone = PhoneNumberField(null=False, blank=False, unique=True)
+    user_name = models.CharField(max_length=200)
+    user_surname = models.CharField(max_length=200)
+    user_birthday = models.DateField()
+
+    def __str__(self):
+        return self.user_name
+
+class Post(models.Model):
+    author = models.CharField(max_length=200)
+    author_email = models.EmailField(max_length=254)
+    publish_date = models.DateField()
+
+
